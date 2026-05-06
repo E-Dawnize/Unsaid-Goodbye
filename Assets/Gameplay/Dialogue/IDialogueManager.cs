097d3124760c4600b6cfe028836ce891239a51da
E-Dawnize <157566656+E-Dawnize@users.noreply.github.com>
2026-05-07 01:54:16 +0800

    测试动画状态机以及Playerview适配状态机

--- Assets/Gameplay/Dialogue/IDialogueManager.cs
+++ Assets/Gameplay/Dialogue/IDialogueManager.cs
@@ -1,9 +1,12 @@
 using System.Threading.Tasks;
+using Gameplay.SO;
 
 namespace Gameplay.Dialogue
 {
     public interface IDialogueManager
     {
+        bool IsPlaying { get; }
         Task PlayAndWait(string dialogueId);
+        Task PlayAndWait(DialogueSequence sequence);
     }
 }
